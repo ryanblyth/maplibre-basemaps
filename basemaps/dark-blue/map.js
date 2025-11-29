37d29054e00aeab2084518300b558ea0a2236395
ryanblyth <ryanjblyth@gmail.com>
2025-11-29 12:26:23 -0700

    feat: add map resource attribution

--- basemaps/dark-blue/map.js
+++ basemaps/dark-blue/map.js
@@ -4,14 +4,25 @@
 const protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
-// Initialize map
+// Initialize map (disable default attribution control)
 const map = new maplibregl.Map({
   container: "map",
   style: "./style.json",
   center: [-105.7821, 39.5501],
   zoom: 8,
-  hash: false
+  hash: false,
+  attributionControl: false
 });
+
+// Add navigation control
+map.addControl(new maplibregl.NavigationControl(), "top-right");
+
+// Add attribution control with custom attribution
+const attributionControl = new maplibregl.AttributionControl({
+  compact: false,
+  customAttribution: "<a href='https://maplibre.org/'>MapLibre</a> | © <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, © <a href='https://openmaptiles.org/'>OpenMapTiles</a> | <a href='https://www.naturalearthdata.com/'>Natural Earth</a>"
+});
+map.addControl(attributionControl);
 
 // Set globe projection when style loads
 map.on('style.load', () => {
@@ -19,9 +30,6 @@ map.on('style.load', () => {
     type: 'globe'
   });
 });
-
-// Add navigation control
-map.addControl(new maplibregl.NavigationControl(), "top-right");
 
 // Log zoom level changes (useful for debugging)
 map.on('zoom', () => {
