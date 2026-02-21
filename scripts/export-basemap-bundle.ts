a0cf29d045511664bfabc87e45e50f98fa7fe504
ryanblyth <ryanjblyth@gmail.com>
2026-02-21 09:08:15 -0700

    feat: add storypath studio attribution

--- scripts/export-basemap-bundle.ts
+++ scripts/export-basemap-bundle.ts
@@ -131,7 +131,7 @@ map.addControl(new maplibregl.NavigationControl(), "top-right");
 // Add attribution control
 const attributionControl = new maplibregl.AttributionControl({
   compact: false,
-  customAttribution: "<a href='https://maplibre.org/'>MapLibre</a> | © <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, © <a href='https://openmaptiles.org/'>OpenMapTiles</a> | <a href='https://www.naturalearthdata.com/'>Natural Earth</a>"
+  customAttribution: "<a href='https://maplibre.org/'>MapLibre</a> | © <a href='https://storypath.studio/'>StoryPath Studio</a> | © <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors | © <a href='https://openmaptiles.org/'>OpenMapTiles</a> | <a href='https://www.naturalearthdata.com/'>Natural Earth</a>"
 });
 map.addControl(attributionControl);
 
@@ -288,7 +288,7 @@ html, body {
     // Add attribution control
     const attributionControl = new maplibregl.AttributionControl({
       compact: false,
-      customAttribution: "<a href='https://maplibre.org/'>MapLibre</a> | © <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, © <a href='https://openmaptiles.org/'>OpenMapTiles</a> | <a href='https://www.naturalearthdata.com/'>Natural Earth</a>"
+      customAttribution: "<a href='https://maplibre.org/'>MapLibre</a> | © <a href='https://storypath.studio/'>StoryPath Studio</a> | © <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors | © <a href='https://openmaptiles.org/'>OpenMapTiles</a> | <a href='https://www.naturalearthdata.com/'>Natural Earth</a>"
     });
     map.addControl(attributionControl);
 
